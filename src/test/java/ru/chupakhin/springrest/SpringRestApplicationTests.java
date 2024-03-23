a5376c990c8007d8835e7988e33ece991b353c1b
Dmitry Chupakhin <chupahkin@gmail.com>
2024-03-23 10:31:41 +0300

    Start Spring App

---
+++ src/test/java/ru/chupakhin/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chupakhin.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
